bce932ed956e039dd0769786dc77f968d0d17e45
Awais Ahmad <160093014+AwaisAhmad211@users.noreply.github.com>
2024-07-20 07:00:49 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ const App = () => {
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          {/* <Navbar />
-          <Hero /> */}
+          <Navbar />
+          <Hero />
         </div>
-        {/* <About />
+        <About />
         <Experience />
-        <Tech /> */}
+        <Tech />
         <Works />
-        {/* <Feedbacks />
+         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
-        </div> */}
+        </div>
       </div>
     </BrowserRouter>
   );
